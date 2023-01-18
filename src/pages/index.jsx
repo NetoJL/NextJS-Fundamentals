7748d5107ab39a25a334dc9707dab46d603a203c
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-18 12:12:07 -0300

    index.jsx updated

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,8 @@
 export default function Home() {
-    return "Home"
+    return (
+        <div>
+            <h1>NextJS and React fundamentals</h1>
+            <h2>Lets study this framework</h2>
+        </div>
+    )
 }
