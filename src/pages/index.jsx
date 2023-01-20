532509545baec72bce6ca2d8fad121b59781e4e1
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-20 13:14:06 -0300

    index.jsx updated (Link added)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
-    return (
-        <div>
-            <h1>NextJS and React fundamentals</h1>
-            <h2>Lets study this framework</h2>
-        </div>
-    )
+	return (
+		<div>
+			<Link href="/stylish">Stylish</Link>
+		</div>
+	);
 }
