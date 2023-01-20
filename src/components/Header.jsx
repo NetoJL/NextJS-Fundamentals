e497698dc3a00aa74cd71ba57a8612a6782e4b9b
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-20 13:09:05 -0300

    Component Header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-export default function Header() {
+export default function Header(props) {
     return (
         <header>
-            <h1>Next.js & React Fundamentals</h1>
+            <h1>{props.title}</h1>
         </header>
     )
 }
