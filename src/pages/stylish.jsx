c73daacdc81ec3afd1e8b66c8f161c54c3dbfcf4
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-20 13:14:21 -0300

    stylish.jsx updated (Link added)

--- src/pages/stylish.jsx
+++ src/pages/stylish.jsx
@@ -1,8 +1,10 @@
 import styles from '../styles/Stylish.module.css'
+import Link from 'next/link'
 
 export default function Stylish() {
     return (
         <div className={styles.purple}>
+            <Link href="/">Back</Link>
             <h1>Style using CSS Modules</h1>
         </div>
     )
