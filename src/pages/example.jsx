c94107aca0498e90717ecb15adb636d85096260f
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-23 15:29:19 -0300

    example.jsx updated

--- src/pages/example.jsx
+++ src/pages/example.jsx
@@ -1,10 +1,11 @@
 import Header from "@/components/Header";
+import Layout from "@/components/Layout";
 
 export default function Example() {
 	return (
-		<>
-			<Header title="Next.js & React"/>
-			<Header title="Learn Next.js in practice "/>
-		</>
+		<Layout title="Using components">
+			<Header title="Next.js & React" />
+			<Header title="Learn Next.js in practice " />
+		</Layout>
 	);
 }
