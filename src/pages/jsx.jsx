e9b38d13076b00abce3487f02925627a86f0bd09
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-23 15:29:55 -0300

    jsx.jsx updated

--- src/pages/jsx.jsx
+++ src/pages/jsx.jsx
@@ -1,18 +1,19 @@
-export default function Jsx(){
+import Layout from "@/components/Layout";
 
-    const title = <h1>JSX is a central concept</h1>
+export default function Jsx() {
+	const title = <h1>JSX is a central concept</h1>;
 
-    function subtitle() {
-        return <h2>{"very nice".toUpperCase()}</h2>
-    }
+	function subtitle() {
+		return <h2>{"very nice".toUpperCase()}</h2>;
+	}
 
-    return (
-        <div>
-            {title}
-            {subtitle()}
-            <p>
-                {JSON.stringify({name: 'Joao', age: 30})}
-            </p>
-        </div>
-    )
+	return (
+		<Layout title="Understanding JSX">
+			<div>
+				{title}
+				{subtitle()}
+				<p>{JSON.stringify({ name: "Joao", age: 30 })}</p>
+			</div>
+		</Layout>
+	);
 }
