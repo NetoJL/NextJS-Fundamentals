1876d5f951ec0cdf1f94bab91021ab4c6e970546
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-23 12:20:51 -0300

    Layout.jsx updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import styles from "../styles/Layout.module.css";
+
+import Header from './Header'
+
 export default function Layout(props) {
 	return (
-		<div>
-            
-        </div>
+		<div className={styles.layout}>
+			<div className={styles.header}>
+                <Header title={props.title ?? 'Header example'}/>
+				<Link href="/">Back</Link>
+			</div>
+
+			<div className={styles.body}>
+                {props.children}
+            </div>
+		</div>
 	);
 }
