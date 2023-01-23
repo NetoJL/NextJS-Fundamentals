735524cecc89c5618d1d9cc7832abe129c8839d7
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-23 15:14:20 -0300

    Navigator.jsx updated

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 export default function Navigator(props) {
     return (
-        <Link href='/stylish'>
-            Stylish
+        <Link href={props.route}>
+            {props.text}
         </Link>
     )
 }
