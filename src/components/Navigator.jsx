57ea0ea060a2a6c48074b60c9d5f65db1b5c0389
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-23 15:29:02 -0300

    Navigator.jsx updated

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,9 +1,15 @@
 import Link from "next/link"
+import styles from '../styles/Navigator.module.css'
 
 export default function Navigator(props) {
     return (
         <Link href={props.route}>
-            {props.text}
+            <div className={styles.navigator} style={{
+                backgroundColor: props.color ?? "dodgerblue"
+            }}>
+                {props.text}
+            </div>
+            
         </Link>
     )
 }
