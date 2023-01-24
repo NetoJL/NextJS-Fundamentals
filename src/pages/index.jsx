a6285bb7ebe8dcba0a0cabc8fcf6a0e7c1f24a98
José Neto <56655916+NetoJL@users.noreply.github.com>
2023-01-24 10:52:23 -0300

    index.jsx updated (state route)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,18 +2,28 @@ import Navigator from "@/components/Navigator";
 
 export default function Home() {
 	return (
-		<div style={{
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			height: '100vh',
-			flexWrap: 'wrap'
-		}}>
-			<Navigator text="Stylish" route="/stylish" color="crimson"/>
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				height: "100vh",
+				flexWrap: "wrap",
+			}}>
+			<Navigator text="Stylish" route="/stylish" color="crimson" />
 			<Navigator text="Example" route="/example" />
 			<Navigator text="JSX" route="/jsx" />
 			<Navigator text="Navigation #01" route="/navigation" />
-			<Navigator text="Navigation #02" route="/customer/pe-2/123" color='green' />
+			<Navigator
+				text="Navigation #02"
+				route="/customer/pe-2/123"
+				color="green"
+			/>
+			<Navigator
+				text="Component with state"
+				route="/state"
+				color="yellow"
+			/>
 		</div>
 	);
 }
